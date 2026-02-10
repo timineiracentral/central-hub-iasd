9772457b99bd3ef6c74ffa24a471607adb18ca41
Felipe Fernandes Jardim <71912649+lipejar@users.noreply.github.com>
2026-02-10 17:05:46 -0300

    build: Set Vite base path to relative './'.

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/",
+  base: "./",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
